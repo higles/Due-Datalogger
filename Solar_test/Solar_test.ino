769a13823fa8f78a349533181dcc099cc38ab4b1
oliv0066 <oliv0066@vandals.uidaho.edu>
2015-04-22 19:08:31 -0700

    Updated Solar code

--- Solar_test/Solar_test.ino
+++ Solar_test/Solar_test.ino
@@ -1,30 +1,38 @@
-int Getsolar();
+float temp = 0;
+float count = 0;
+int timer = 0;
+
+float Getsolar();
 
 void setup() {
   // put your setup code here, to run once:
-    pinMode( 80 , INPUT);
-    Serial.begin(9600);
-    analogReadResolution(12);
-    
+    pinMode( 56 , INPUT);
+    Serial.begin(115200);
+    analogReadResolution(13); 
+    timer = millis();   
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-    double x;
-    
-    x = Getsolar();
-    Serial.print("voltage: ");
-    Serial.println(x);
-    
+  // put your main code here, to run repeatedly:    
+    temp += Getsolar();
+    if (millis() - timer > 1000) {
+      temp = temp/count;
+      timer = millis();
+      count = 0;
+      Serial.print("voltage: ");
+      Serial.println(temp);
+      temp = 0;
+    }
 }
 
 
 
-int Getsolar(){
-  
-    double temp;
-    temp = analogRead(8);
-    temp = temp/819;
-    return temp;
+float Getsolar(){
+    float t;
+    t = (float)analogRead(56);
+    t = t*100.00;
+    t = t/1241.00;
+    count++;
+    return t;
     
 }
